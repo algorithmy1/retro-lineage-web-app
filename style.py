8888d8fa124cff26cab5cc3d94a3deab77330853
Abdallah Terrab <abdallah.terrab.partner@decathlon.com>
2021-06-01 12:21:43 +0200

    working !

---
+++ style.py
@@ -0,0 +1,69 @@
+
+DECATHLON_LOGO = "https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Decathlon_Logo.png/799px-Decathlon_Logo.png"
+
+LOGO_BAR_STYLE = {
+    "display":"block",
+    "margin-left":"auto",
+    "margin-right":"auto"
+}
+
+SIDEBAR_STYLE = {
+    "position": "fixed",
+    "top": 0,
+    "left": 0,
+    "bottom": 0,
+    "width": "16rem",
+    "padding": "2rem 1rem",
+    "background-color": "#f8f9fa",
+}
+
+CONTENT_STYLE = {
+    "margin-left": "18rem",
+    "margin-right": "2rem",
+    "padding": "2rem 1rem",
+}
+
+NODE_STYLE={
+    'shape':'roundrectangle',
+    'height': 60,
+    'width': 200,
+    'border-color': '#ffffff',
+    'border-width': 1,
+    'border-opacity': 0.5,
+    'border-radius': '30%',
+    'background-color': '#ffffff',
+    'border-color' : '#000000',
+    'text-color': '#ffffff',
+    'text-wrap': 'wrap',
+    'text-halign': 'center',
+    'text-valign': 'center',
+    'label': 'data(label)',
+}
+
+EDGE_STYLE = {
+    'curve-style': 'bezier', # 'unbundled-bezier',
+    'target-arrow-shape': 'vee',
+    'line-color': '#000000',
+    'target-arrow-color': '#000000',
+    'width': 2
+}
+
+INPUT_STYLE = {
+    'background-color': '#2D9BF0',
+    'color': '#ffffff'
+}
+
+INTERMEDIATE_STYLE = {
+    'background-color': '#DA0063',
+    'color': '#ffffff'
+}
+
+TEMPORARY_STYLE = {
+    'background-color': '#F24726',
+    'color': '#ffffff'
+}
+
+PERMANENT_STYLE = {
+    'background-color': '#FFA500',
+    'color': '#ffffff'
+}
